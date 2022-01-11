2ff12e061e8e86718560bce59175b5e721216d58
heejinkim908 <71063574+CreamNuna@users.noreply.github.com>
2022-01-12 02:24:01 +0900

    feat: Add chat with websockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,42 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+const socket = new WebSocket(`ws://${window.location.host}`); //front-backend 서버로 연결
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg); //object를 string으로 바꿔 backend에 전송
+}
+
+socket.addEventListener("open", ()=>{
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message)=>{
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+}); //server한테 메제지 받음
+
+socket.addEventListener("close", ()=>{
+    console.log("Disconnected from Server ❌");
+});
+
+// setTimeout(()=>{
+//     socket.send("hello from the browser");
+// }, 10000); //server한테 메세지 보냄
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value)); //server한테 메세지 보냄
+    input.value="";
+}
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value)); //server한테 닉네임 보냄
+    input.value="";
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,51 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 // express로 view와 render 설정 
 
 const app = express();
 
-
 app.set("view engine", "pug"); //pug로 view engine 설정
 app.set("views", __dirname + "/views"); //express에 template이 어디있는지 지정
-app.use("/public", express.static(__dirname + "/public")); //public url 생성해서 유저에게 파일 공유(app.js)
-
+app.use("/public", express.static(__dirname + "/public")); //public url 생성해서 유저에게 파일 공유(app.js): front
+ 
 app.get("/", (req, res) => res.render("home")); //route handler로 home.pug render해줌
+app.get("/*", (req, res) => res.redirect("/")); //어떤 페이지로 GET request 보내도 redirect로 반응
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+//http, websocket protocol 생성
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+//socket: 연결된 브라우저
+wss.on("connection", (socket) => {
+    sockets.push(socket); //연결된 브라우저 모음 
+    
+    socket["nickname"] = "Anonymous";
+
+    console.log("Connected to Browser ✅");
+    
+    socket.on("close", ()=>{
+       console.log("Disconnected from the Browser ❌");
+    });
+
+    socket.on("message", (msg)=>{
+        //console.log(message.toString('utf8')); //socket한테 메세지 받은거 출력
+        const message = JSON.parse(msg); //string을 object로 전환
+        switch(message.type){
+            case "new_message":
+                sockets.forEach( (aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`)); //socket한테 받은 메세지 다시 받아쳐서 모든 브라우저에 보냄
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
+        //socket.send(message.toString('utf8')); //socket한테 받은 메세지 다시 받아침
+    }); 
+
+    //socket.send("hello!!"); //socket한테 메세지 보냄
+});
+
+server.listen(3000, handleListen);
